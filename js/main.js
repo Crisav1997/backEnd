a14f59fc243f378a5bb74b9de8cd5aee07b9ca6b
crisav <avilacris77@gmail.com>
2023-04-03 15:10:56 -0300

    Se trabaja en Static

--- js/main.js
+++ js/main.js
@@ -4,11 +4,15 @@ class Usuario{
     this.apellido=datos.apellido
     this.edad=datos.edad
     this.contador= 0;
+    Usuario.contador++
     }
+    static contador = 0
 ver(){
     console.log(this.nombre)
 }
 }
 let Usuario1 =  new Usuario({nombre:"cristian",apellido:"avila",edad:25});
-console.log(Usuario1)
+let Usuario2 =  new Usuario({nombre:"cristian",apellido:"avila",edad:25});
+let Usuario3 =  new Usuario({nombre:"cristian",apellido:"avila",edad:25});
+console.log(Usuario.contador)
 Usuario1.ver()
