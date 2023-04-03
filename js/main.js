6679471f3701e1dcb23fbd14c66cb4c9cf21d1d1
Crisav <avilacris77@gmail.com>
2023-04-02 22:29:43 -0300

    se trabaja con clases

--- js/main.js
+++ js/main.js
@@ -1,2 +1,14 @@
-console.log("hola mundo")
-let name= prompt("ingrese su name")
+class Usuario{
+    constructor(datos){
+    this.nombre=datos.nombre
+    this.apellido=datos.apellido
+    this.edad=datos.edad
+    this.contador= 0;
+    }
+ver(){
+    console.log(this.nombre)
+}
+}
+let Usuario1 =  new Usuario({nombre:"cristian",apellido:"avila",edad:25});
+console.log(Usuario1)
+Usuario1.ver()
