f2207579b6bb46ae0d33d41a4e1126e9789a2721
crisav <avilacris77@gmail.com>
2023-04-10 16:07:32 -0300

    Se limpia Codigo

--- js/sprint-1.js
+++ js/sprint-1.js
@@ -8,13 +8,11 @@ class ProductManager{
     }
     getProductById(IdFind){
         let get = this.products.find(e => e.id === IdFind);
-        if(get===undefined)console.error("no existe ID")
+        if(get===undefined)console.error("Not found")
         else{console.log(get)}
     }
-    addProduct({title,description,price,thumbnail,code,stock}){
-    let flag=0;
-    let codeIn=this.products.find(e=> e.code===code)
-    if(codeIn===undefined){
+    addProduct({title,description,price,thumbnail,stock}){
+        let flag=0;
         let id = 0
         if(this.products.length===0){
             id = 1 
@@ -23,22 +21,18 @@ class ProductManager{
             let lastProducts = this.products[this.products.length-1]
             id=lastProducts.id + 1
             }
-        let product={ title,description,price,thumbnail,code,stock,id }
+        let product={ title,description,price,thumbnail,stock,id }
         for(const value in product){
             if((product[value]==null)||(product[value]=="")){
             flag=1;} 
             }
         if(flag===0){this.products.push(product)}
         else{console.log("Hay algun elemento sin completar")}
-        }
-    else{
-        console.error("ID existente")
-        }
     }
 }
 let product= new ProductManager()
-product.addProduct({title:"Agua",description:"100% Natural",price: 150,thumbnail:"img1",code:"123456789",stock:100 })
-product.addProduct({title:"CocaCola",description:"sin azucar",price:25,thumbnail:"img2",code:"987654321",stock:150 })
-product.addProduct({title:"speed",description:"Energizante",price: 100,thumbnail:"img3",code:"123789456",stock:80 })
+product.addProduct({title:"Agua",description:"100% Natural",price: 150,thumbnail:"img1",stock:100 })
+product.addProduct({title:"CocaCola",description:"sin azucar",price:25,thumbnail:"img2",stock:150 })
+product.addProduct({title:"speed",description:"Energizante",price: 100,thumbnail:"img3",stock:80 })
 product.getProducts()
 product.getProductById(3)
