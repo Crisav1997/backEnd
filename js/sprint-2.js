398294e215c53a82ed5fec51a52d29dfc1eba596
crisav <avilacris77@gmail.com>
2023-04-22 18:03:22 -0300

    Se limpia codigo, falta addProduct

--- js/sprint-2.js
+++ js/sprint-2.js
@@ -19,7 +19,6 @@ class ProductManager{
             return 'data recovered'
         }
     }
-
     async addProduct({title,description,price,thumbnail,stock}){
         try {
             let data = { title,description,price,thumbnail,stock }
@@ -43,62 +42,51 @@ class ProductManager{
     async getProducts() {
         try{
            if(this.products.length){
-            console.log(this.products.length)
             return this.products
            }else{
-            //console.log("Not found")
             return 'Not found' 
            }     
         }catch(error){
-            console.log(error)
-            return 'getProducts: error'
+            return 'getProducts:error'
         }  
     }
     async getProductById(id) {
         try{
         let search = this.products.find(each=>each.id===id)
         if(search){
-            console.log(search)
             return search
         }else{
-            //console.log("Not found")
             return "Not found"
         }}
         catch(error){
-            console.log("erros")
+            return 'getProductById: error'
         }
     }
-    async update_user(id,data) {
-    
+    async updateProduct(id,data) {
         try {
             let search =await this.getProductById(id)
-        
-
              if(search==="Not found"){
-                 console.log("No existe ID")
+                console.log("No existe ID")
              }else{
-             console.log("sigamos")
                  for (let prop in data) {
                      search[prop] = data[prop]
                  }
-                 //convierto a texto plano el array
                  let data_json = JSON.stringify(this.products,null,2)
-                 //sobre-escribo el archivo
                  await fs.promises.writeFile(this.path,data_json)
-                 console.log('updated user: '+id)
+                 console.log('updateProduct: done , '+id)
                  return 'updated user: '+id
-             }
-            
-        } catch(error) {
-            console.log(error)
-            return 'error: updating user'
-        }
-    }
-    async destroy_user(id) {
+             }} 
+             catch(error) {
+                console.log(error)
+                return 'updateProduct: error'
+            }
+         }
+    async deleteProduct(id) {
         try {
             let search =await this.getProductById(id)
             if(search==="Not found"){
                 console.log("No existe ID")
+                return "Not found"
              }else{
             this.products = this.products.filter(each=>each.id!==id)
             console.log(this.products)
@@ -113,20 +101,19 @@ class ProductManager{
     }
     
 }
-
 async function manager() {
     let manager = new ProductManager('./products.json')
-    //await manager.addProduct({ title:'igna',description:'borraz',price:32,thumbnail:[],stock:0 });
+   // await manager.addProduct({ title:'igna',description:'borraz',price:32,thumbnail:[],stock:0 });
     await manager.getProducts()
     //await manager.getProductById(1)
    // await manager.addProduct({ title:'cris',description:'avila',price:12,thumbnail:[],stock:41})
     // await manager.add_user({ name:'igna',last_name:'chapero',age:100,carts:[] })
     // await manager.add_user({ name:'mario',last_name:'castro',age:35,carts:[] })
     // await manager.add_user({ name:'luis',last_name:'aguilar',age:35,carts:[] })
-    //await manager.update_user(2,{ title:'andrew' })
+    //await manager.updateProduct(2,{ title:'probando' })
     // await manager.update_user(2,{ name:'nicolas', carts: ['celular'] })
     // await manager.update_user(3,{ age:30 })
-    //await manager.destroy_user(2)
-    // await manager.destroy_user(4)
+    //await manager.deleteProduct(2)
+    // await manager.deleteProduct(4)
 }
 manager()
