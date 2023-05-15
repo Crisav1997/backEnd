cee7610afeba5d97d8dadc41d7edd2d1bca236c7
crisav <avilacris77@gmail.com>
2023-05-15 15:18:49 -0300

    working

---
+++ src/routes/api/index.js
@@ -0,0 +1,1 @@
+//llama a recursos
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,5 +4,7 @@ import api_router from './api/'
 
 const index_router = Router()
 
+index_router.use('/api',api_router)
+index_router.use('/',views_router)
 
 export default router
